faece276e45f42e20714410c212bd480caa13c20
Gauthmi <gauthamiv75@gmail.com>
2022-07-02 17:02:52 +0530

    link Updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <a href='https://upload-files-project.s3.amazonaws.com/CSS-Framework-Introducing-Bootstrap.pdf'>Links</a>
+      <a href='https://upload-files-project.s3.amazonaws.com/Bank_Details_14.02.2022_50007.xlsx'>Links</a>
     </div>
   );
 }
